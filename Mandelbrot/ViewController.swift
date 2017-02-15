28947ec32ee023485a86a53837f0f6e561188075
Richard Simko <richard@refinedwiki.com>
2017-02-15 12:40:12 +0100

    Initial Commit

---
+++ Mandelbrot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mandelbrot
+//
+//  Created by Richard Simko on 2017-02-15.
+//  Copyright © 2017 Richard Simko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
